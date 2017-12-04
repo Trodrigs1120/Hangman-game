b43addd6dc35598900505ca7559fcb049d2ff67d
trodrigs1120 <trodrigs@yahoo.com>
2017-12-04 16:51:08 -0500

    working on loops

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,10 +14,10 @@ document.onkeyup = function(event) {
   
     console.log(PlayerGuess);
     //console.log(StartGame);
-    if (PlayerGuess==="Enter" && StartGame===0){
+    if (StartGame===0){
         StartGame=1
         if (StartGame=1){
-         Word=WordArray[Math.floor((Math.random() * WordArray.length) + 1)];
+        Word=WordArray[Math.floor((Math.random() * WordArray.length) + 1)];
     //  console.log(WordChosen)  
        console.log(Word); // Debugging to check the hangman word
         // console.log(UnderScores);  Debugging to check word length
@@ -30,22 +30,25 @@ document.onkeyup = function(event) {
         AnswerKey[i]=Word.charAt(i);
         console.log(AnswerKey[i]);    
         }
-        for (var i=0; i < Word.length; i++ || exitloop===1){
-        if (AnswerKey[i]===PlayerGuess){
-            exitloop=1; // loop will cease 
-                        // I need code to replace an _ with a successful guess IE If miami is the word
-                        // an I guess should result in _ i _ _ i
-
-        }    
-        }
+        
+        
         // We'll need an If statement where if exitloop=0 then the player loses a life
-        console.log(exitloop)
+        
         console.log(UnderScores);
        HangManOutPut.innerText = UnderScores; 
        HangMan.appendChild(HangManOutPut);
 
         };
     };
+    for (var i=0; i < Word.length; i++ || exitloop===1){
+        if (AnswerKey[i]===PlayerGuess){
+            exitloop=1; // loop will cease 
+            console.log(exitloop);        
+            // I need code to replace an _ with a successful guess IE If miami is the word
+            // an I guess should result in _ i _ _ i
+    }; 
+      console.log(i);
+    }
     // 
 //    
      
