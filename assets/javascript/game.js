2d63459c58467dba6959e9fe7b6f29ce44d82c2e
trodrigs1120 <trodrigs@yahoo.com>
2017-12-05 19:18:32 -0500

    updated comments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,10 @@
 var WordArray=["miami", "transformers","arcade","cassette", "pong"];
-var GuessesLeft;
-var WinStreak;
-var StartGame=0;
+var GuessesLeft; // not used yet
+var WinStreak; // not used yet 
+var StartGame=0; // variable for intial run 
 var AnswerKey=[""];
 var Word="";
 var exitloop;
-
-// remove this bullshit later
-var wins = 0;
-var losses = 0;
-var ties = 0;
-var computerChoices = ["r", "p", "s"];
 var UnderScores = [""];
 
 var HangMan = document.getElementById("HangMan");
@@ -21,7 +15,7 @@ document.onkeyup = function(event) {
   var PlayerGuess = event.key;   
     
 function Redraw(){
-  var UnderScoresOutput="";
+  var UnderScoresOutput=""; // without this it adds to itself endlessly
  for (var i=0; i< Word.length; i++){ 
     
     //  console.log(UnderScores[i]);
@@ -32,14 +26,10 @@ function Redraw(){
   var html = UnderScoresOutput;
   document.querySelector("#HangMan").innerHTML = html;
 }
-    
               // Determines which key was pressed.
               var userGuess = event.key;
               if (StartGame===0){
-                
-                
                 Word=WordArray[Math.floor((Math.random() * WordArray.length) + 1)];
-                
                console.log(Word); // Debugging to check the hangman word
                 // console.log(UnderScores);  Debugging to check word length
                 for (var i=0; i< Word.length; i++){ 
@@ -54,16 +44,15 @@ function Redraw(){
                 StartGame=1 
                 for (var i=0; i < Word.length; i++){
                   AnswerKey[i]=Word.charAt(i);
-                  console.log(AnswerKey[i]);    
+                //  console.log(AnswerKey[i]);    
                   }
               }
-            
+            // checks to see if the player guess matches the answer key at all
             for (var i=0; i < Word.length; i++){
               if (AnswerKey[i]===PlayerGuess){
                   UnderScores[i] = AnswerKey[i];
                   console.log(AnswerKey[i]);
-            
                                             };
                                               };
-        Redraw();
+        Redraw(); // redraws the _ _ _ _ 
       };
